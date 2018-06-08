bf2eed959a11c30d2a27771ce4e6969da451111f
Amit Saha <amitsaha@users.noreply.github.com>
2018-06-08 10:57:27 +1000

    Update infrastructure.tf

--- terraform/configuration/infrastructure.tf
+++ terraform/configuration/infrastructure.tf
@@ -1,15 +1,7 @@
-provider aws {
-  region = "ap-southeast-2"
-  profile = "dev"
-  version = "~> 1.5"
-}
-
-resource "aws_s3_bucket" "b" {
-  bucket_prefix = "consul-tf-demo"
-  acl    = "private"
-
-  tags {
-    Name        = "My bucket"
-    Environment = "Dev"
+resource "consul_keys" "test" {  
+ 
+  key {
+    path  = "app1/version"
+    value = "0.1"
   }
 }
